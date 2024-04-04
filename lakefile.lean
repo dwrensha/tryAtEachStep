c33745d510453321a325e314aaf691a6eef516ea
David Renshaw <dwrenshaw@gmail.com>
2024-04-04 07:52:55 -0400

    lean v4.7.0

--- lakefile.lean
+++ lakefile.lean
@@ -8,7 +8,7 @@ package «tryAtEachStep» where
     ⟨`relaxedAutoImplicit, false⟩
   ]
 
-require std from git "https://github.com/leanprover/std4" @ "main"
+require std from git "https://github.com/leanprover/std4" @ "v4.7.0"
 
 @[default_target]
 lean_exe «tryAtEachStep» where
